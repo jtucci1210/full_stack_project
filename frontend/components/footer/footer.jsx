e95141e76300e6ef79ff79e629a82382a0e43d82
jptucci <jptucci1210@gmail.com>
2020-01-15 16:30:34 -0800

    Change footer links to open new tab when clicked on

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="footer-div">
             <h4 className="made-by-statement">FeetUp was made ever-so-lovingly by JP Tucci</h4>
             <div className="footer-logos">
-                <a href="https://github.com/jtucci1210"><i className="fab fa-github"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/jtucci1210"><i className="fab fa-github"></i></a>
                 <div className="im-a-spacer"></div>
-                <a href="https://www.linkedin.com/in/james-tucci-56b06b99/"><i className="fab fa-linkedin"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/james-tucci-56b06b99/"><i className="fab fa-linkedin"></i></a>
             </div>
         </div>
     )
